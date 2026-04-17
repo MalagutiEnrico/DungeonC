639726ff97c1497acbd5897d04d9819d7dafe225
Malaguti Enrico <enrico.malaguti@galileiostiglia.edu.it>
2026-04-17 08:12:23 +0000

    riorganizzazione dei file nelle cartelle

--- DungeonC/include/tipi.h
+++ DungeonC/include/tipi.h
@@ -3,6 +3,8 @@
 #define MAX_INVENTARIO 8        //numero di oggetti massimi nell'inventario
 #define MAX_DIMINPUT 100        //numero massimo di caratteri inseribili in fase di input
 #define MAX_SALUTE 100          //valore massimo di salute dell'eroe
+#define MAX_OGGETTI 5           //numero massimo di oggetti in una stanza
+#define DIREZIONI 4             //numero massimo di stanze accessibili da una stanza
 
 //Definizione dei tipi di oggetto presenti nelle stanze e nell'inventario
 typedef enum{
@@ -10,7 +12,8 @@ typedef enum{
     ARMA,
     ARMATURA, 
     CHIAVE,
-    TORCIA
+    TORCIA,
+    NUM_OGGETTI
 }TipoOggetto;
 
 //Definizione dei tipi di mostro incontrabili nel gioco
@@ -18,7 +21,8 @@ typedef enum{
     SCHELETRO,
     GOBLIN,
     DRAGO,
-    BOSS
+    BOSS,
+    NUM_MOSTRI
 }TipoMostro;
 
 //Definizione dei tipi di comando inseribili nel gioco
@@ -72,16 +76,27 @@ typedef struct Stanza{
     int numero_mostri;
 }Stanza;
 
+typedef struct{
+    Stanza* inizio;
+    int numero_stanze;
+}Mappa;
+
 //struttura dati pila inventario eroe
 typedef struct{
     Oggetto* next;
     int len;
-}Pila;
+}Inventario;
 
 //struttura per l'eroe
 typedef struct{
     int HP;
     int XP;
-    Pila* inventario;
+    Inventario* inventario;
     Stanza* stanza_corrente;
 }Eroe;
+
+/**
+ * Funzione che controlla l'esito dell'allocazione di uno spazio di memoria. In caso sia fallito esce dal programma
+ * @param void* puntatore da controllare
+*/
+void controlla_allocazione(void* ptr);
