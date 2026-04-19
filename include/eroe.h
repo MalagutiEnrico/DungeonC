cf3772bcc66ecb1185e3a624d54aba8dc21e07c4
Malaguti Enrico <malagutienrico08@gmail.com>
2026-04-19 16:28:31 +0200

    configurazione git

---
+++ include/eroe.h
@@ -0,0 +1,14 @@
+/*Libreria per la gestione dell'eroe nel gioco*/
+#include "tipi.h"
+
+/**
+ * Funzione che crea un inventario di oggetti
+ * @return Inventario* puntatore all'inventario
+*/
+Inventario* crea_inventario();
+
+/**
+ * Funzione che crea un eroe
+ * @return Eroe* puntatore all'eroe
+*/
+Eroe* crea_eroe();
